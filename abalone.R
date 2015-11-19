f37bb10a35f47909a9ac81d94069896b4d3b8d5b
LuoShaozhuo <752085135@qq.com>
2015-11-19 15:05:04 +0800

    test

---
+++ abalone.R
@@ -0,0 +1,9 @@
+abalone = read.csv("data/abalone.csv")
+table(abalone$Sex)
+plot(Length ~ Sex, data = abalone)
+meanLength <- mean(abalone$Length)
+model <- lm(Whole.weight ~ Length + Sex, data=abalone)
+x <- 1:3
+cv <- function(x, na.rm=FALSE){
+  sd(x, na.rm=na.rm)/mean(x, na.rm=na.rm)
+} 
